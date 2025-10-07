0fc3023c5bb2687e2ab05cbf3df7d491eb93dd14
fashionaisoftware <fashionaisoftware@gmail.com>
2025-10-07 22:23:01 +0200

    just updated the main page.tsx to have basic styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Jesus is King</h1>
-      <p>16 For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.</p>
+    <div className="flex flex-col justify-center py-24 ">
+      <h1 className="mx-auto font-bold text-5xl text-gray-700">Jesus is King</h1>
+      <p className="mx-auto pt-12 max-w-md text-2xl text-gray-500">16 For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.</p>
     </div>
   );
 }
